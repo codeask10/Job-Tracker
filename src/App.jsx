4e6a97c776ccf8b2b0bbb2f12836ce4b3589a0d7
codeask10 <annajmussaquib123@gmail.com>
2024-10-19 22:07:01 +0530

    Initial commit with project setup, configuration files, routing and homepage

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+HomePage
+import MainLayouts from './layouts/MainLayouts';
+import JobPage from './pages/JobPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path='/' element={<MainLayouts />}>
+    <Route index element={<HomePage />} />
+    <Route path='/jobs' element={<JobPage />} />
+    <Route path='*' element={<NotFoundPage />} />
+  </Route>)
+);
+const App = () => {
+
+  return (
+    <RouterProvider router={router} />
+
+  )
+}
+
+export default App
