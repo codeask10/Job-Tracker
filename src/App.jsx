117efcf0bb8b15dfcf9cdd27fc61e4c0c6ef0fb7
codeask10 <annajmussaquib123@gmail.com>
2024-10-20 10:30:37 +0530

    feat: add job page component, implement job submission, and integrate into app router

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,29 @@ import MainLayouts from './layouts/MainLayouts';
 import JobPage from './pages/JobPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobDetailsPage, { jobLoader } from './pages/JobDetailsPage';
+import AddJobPage from "./pages/AddJobPage";
 
-
-const router = createBrowserRouter(
-  createRoutesFromElements(<Route path='/' element={<MainLayouts />}>
-    <Route index element={<HomePage />} />
-    <Route path='/jobs' element={<JobPage />} />
-    <Route path='/jobs/:id' element={<JobDetailsPage />} loader={jobLoader} />
-    <Route path='*' element={<NotFoundPage />} />
-  </Route>)
-);
 const App = () => {
+
+  const newJob = async (newJob) => {
+    const res = await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        'content-type': "application/json"
+      },
+      body: JSON.stringify(newJob),
+    });
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(<Route path='/' element={<MainLayouts />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobPage />} />
+      <Route path='/jobs/:id' element={<JobDetailsPage />} loader={jobLoader} />
+      <Route path='/add-job' element={< AddJobPage addJobSubmit={newJob} />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>)
+  );
 
   return (
     <RouterProvider router={router} />
