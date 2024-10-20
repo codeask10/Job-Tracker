3a2ee3ac2ec7c0ba74a889a684675b8d425132e5
codeask10 <annajmussaquib123@gmail.com>
2024-10-20 09:44:40 +0530

    Update path for single job with id

--- src/Components/JobListing.jsx
+++ src/Components/JobListing.jsx
@@ -33,7 +33,7 @@ const JobListing = ({ job }) => {
                         {job.location}
                     </div>
                     <Link
-                        to={`/Job/${job.id}`}
+                        to={`/Jobs/${job.id}`}
                         className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                     >
                         Read More
